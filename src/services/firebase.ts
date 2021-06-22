4e1f1890a1f9a63906618717581ae42bad1e0b2f
GilbertoASJ <alvesgilberto84@gmail.com>
2021-06-22 15:01:09 -0300

    :sparkles: Adding firebase to project

---
+++ src/services/firebase.ts
@@ -0,0 +1,23 @@
+// Importando a biblioteca firebase, instalada via 'yarn add firebase'
+import firebase from 'firebase';
+
+// Importando os seviços que irão ser utilizados do firebase
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+// Inicializando o app
+firebase.initializeApp(firebaseConfig);
+
+// Exportando funções
+export const auth = firebase.auth();
+export const database = firebase.database();
