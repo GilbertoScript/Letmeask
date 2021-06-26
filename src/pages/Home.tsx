b5782287628a27ec456218103b98bc8bfeeeaf84
GilbertoASJ <alvesgilberto84@gmail.com>
2021-06-26 17:31:27 -0300

    :sparkles: Updating files

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,7 +28,7 @@ export function Home() {
 	}
 
 	async function handleJoinRoom(event: FormEvent) {
-
+							   
 		event.preventDefault();
 
 		if(roomCode.trim() == '') {
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,10 +1,12 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'
 import { useAuth } from '../hooks/useAuth';
+import { useRoom } from '../hooks/useRoom'
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
 import { database } from '../services/firebase';
+import { Question } from '../components/Question';
 
 import logoImg from '../assets/logo.svg'
 
@@ -22,6 +24,7 @@ export function Room() {
 	const roomId = params.id;
 
 	const [ newQuestion, setNewQuestion ] = useState('');
+	const { questions, title } = useRoom(roomId)
 
 	async function handleSendQuestion(event: FormEvent) {
 
@@ -61,8 +64,8 @@ export function Room() {
 
 			<main>
 				<div className="room-title">
-					<h1>Sala de react</h1>
-					<span>4 perguntas</span>
+					<h1>Sala: {title}</h1>
+					{ questions.length > 0 && <span>{questions.length} pergunta(s)</span> }
 				</div>
 
 				<form onSubmit={handleSendQuestion}>
@@ -87,6 +90,18 @@ export function Room() {
 						<Button type="submit" disabled={!user}>Enviar pergunta</Button>
 					</div>
 				</form>
+
+				<div className="question-list">
+					{questions.map(question => {
+						return (
+							<Question 
+								key={question.id} 
+								content={question.content} 
+								author={question.author} 
+							/>
+						)
+					})}
+				</div>
 			</main>
 		</div>
 	)
